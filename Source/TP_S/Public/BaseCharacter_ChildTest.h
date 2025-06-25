b7c5d435b06ecbaf2d8533f41b273fd728c956a3
audwns0831 <suitking@naver.com>
2025-06-25 13:41:29 +0900

    TEST

---
+++ Source/TP_S/Public/BaseCharacter_ChildTest.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseCharacter.h"
+#include "BaseCharacter_ChildTest.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TP_S_API ABaseCharacter_ChildTest : public ABaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Commit TEst
+	
+};
