f3e5198523cac9648c0b82f3417ffbb231dfcbe8
TAlaugh <powe556@gmail.com>
2025-06-25 14:10:56 +0900

    Update BaseCharacter.cpp

--- Source/TP_S/Private/BaseCharacter.cpp
+++ Source/TP_S/Private/BaseCharacter.cpp
@@ -7,7 +7,7 @@
 ABaseCharacter::ABaseCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 }
 
