0e1208b6317a0724e5bac6b7b7422e6dbcc1a450
audwns0831 <suitking@naver.com>
2025-06-25 13:49:04 +0900

    Test2

---
+++ Source/TP_S/Public/Test2.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseCharacter.h"
+#include "Test2.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TP_S_API ATest2 : public ABaseCharacter
+{
+	GENERATED_BODY()
+	
+};
