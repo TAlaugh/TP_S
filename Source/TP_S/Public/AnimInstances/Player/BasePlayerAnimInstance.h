de36d218c56474e7bebfa992bae3a3130aa634d0
audwns0831 <suitking@naver.com>
2025-06-26 16:58:52 +0900

    BaseSetting

---
+++ Source/TP_S/Public/AnimInstances/Player/BasePlayerAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AnimInstances/BaseAnimInstance.h"
+#include "BasePlayerAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TP_S_API UBasePlayerAnimInstance : public UBaseAnimInstance
+{
+	GENERATED_BODY()
+	
+};
