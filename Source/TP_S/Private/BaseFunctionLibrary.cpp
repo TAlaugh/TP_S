39d77fdc2a3ce53a69977e351622b61c7ba9db33
TAlaugh <powe556@gmail.com>
2025-06-27 12:01:10 +0900

    0627 1201

--- Source/TP_S/Private/BaseFunctionLibrary.cpp
+++ Source/TP_S/Private/BaseFunctionLibrary.cpp
@@ -2,4 +2,140 @@
 
 
 #include "BaseFunctionLibrary.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "BaseGameplayTags.h"
+#include "DebugHelper.h"
+#include "GenericTeamAgentInterface.h"
+#include "AbilitySystem/BaseAbilitySystemComponent.h"
+#include "Kismet/KismetMathLibrary.h"
 
+UBaseAbilitySystemComponent* UBaseFunctionLibrary::NativeGetBaseASCFromActor(AActor* InActor)
+{
+	check(InActor);
+
+	return CastChecked<UBaseAbilitySystemComponent>(UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(InActor));
+
+}
+
+void UBaseFunctionLibrary::AddGameplayTagToActorIfNone(AActor* InActor, FGameplayTag TagToAdd)
+{
+	UBaseAbilitySystemComponent* ASC = NativeGetBaseASCFromActor(InActor);
+
+	if (!ASC->HasMatchingGameplayTag(TagToAdd))
+	{
+		ASC->AddLooseGameplayTag(TagToAdd);
+	}
+}
+
+void UBaseFunctionLibrary::RemoveGameplayTagFromActorIfFound(AActor* InActor, FGameplayTag TagToRemove)
+{
+	UBaseAbilitySystemComponent* ASC = NativeGetBaseASCFromActor(InActor);
+
+	if (ASC->HasMatchingGameplayTag(TagToRemove))
+	{
+		ASC->RemoveLooseGameplayTag(TagToRemove);
+	}
+}
+
+bool UBaseFunctionLibrary::NativeDoesActorHaveTag(AActor* InActor, FGameplayTag TagToCheck)
+{
+	UBaseAbilitySystemComponent* ASC = NativeGetBaseASCFromActor(InActor);
+
+	return ASC->HasMatchingGameplayTag(TagToCheck);
+}
+
+void UBaseFunctionLibrary::BP_DoesActorHaveTag(AActor* InActor, FGameplayTag TagToCheck,
+	EBaseConfirmType& OutConfirmType)
+{
+	OutConfirmType = NativeDoesActorHaveTag(InActor, TagToCheck) ? EBaseConfirmType::Yes : EBaseConfirmType::No;
+}
+
+// UPawnCombatComponent* UBaseFunctionLibrary::NativeGetPawnCombatComponentFromActor(AActor* InActor)
+// {
+// 	check(InActor);
+//
+// 	if (IPawnCombatInterface* PawnCombatInterface = Cast<IPawnCombatInterface>(InActor))
+// 	{
+// 		return PawnCombatInterface->GetPawnCombatComponent();
+// 	}
+// 	
+// 	return nullptr;
+// }
+//
+// UPawnCombatComponent* UBaseFunctionLibrary::BP_GetPawnCombatComponentFromActor(AActor* InActor,
+// 	EBaseValidType& OutValidType)
+// {
+// 	UPawnCombatComponent* CombatComponent = NativeGetPawnCombatComponentFromActor(InActor);
+//
+// 	OutValidType = CombatComponent ? EBaseValidType::Valid : EBaseValidType::InValid;
+//
+// 	return CombatComponent;
+// }
+
+// bool UBaseFunctionLibrary::IsTargetPawnHostile(APawn* QueryPawn, APawn* TargetPawn)
+// {
+// 	check(QueryPawn && TargetPawn);
+// 	
+// 	IGenericTeamAgentInterface* QueryTeamAgent = Cast<IGenericTeamAgentInterface>(QueryPawn->GetController());
+// 	IGenericTeamAgentInterface* TargetTeamAgent =Cast<IGenericTeamAgentInterface>(TargetPawn->GetController());
+//
+// 	if (QueryTeamAgent &&  TargetTeamAgent)
+// 	{
+// 		return QueryTeamAgent->GetGenericTeamId() != TargetTeamAgent->GetGenericTeamId();
+// 	}
+// 	return false;
+// }
+//
+// float UBaseFunctionLibrary::GetScalableFloatValueAtLevel(const FScalableFloat& InScalableFloat, float InLevel)
+// {
+// 	return InScalableFloat.GetValueAtLevel(InLevel);
+// }
+
+// FGameplayTag UBaseFunctionLibrary::ComputeHitReactDirectionTag(AActor* InAttacker, AActor* InVictim,
+// 	float& OutAngleDifference)
+// {
+// 	check(InAttacker && InVictim);
+//
+// 	const FVector VictimForward = InVictim->GetActorForwardVector();
+// 	const FVector VictimToAttackerNormalized = (InAttacker->GetActorLocation() - InVictim->GetActorLocation()).GetSafeNormal();
+//
+// 	const float DotResult = FVector::DotProduct(VictimForward, VictimToAttackerNormalized);
+// 	OutAngleDifference = UKismetMathLibrary::DegAcos(DotResult);
+//
+// 	const FVector CrossResult = FVector::CrossProduct(VictimForward, VictimToAttackerNormalized);
+//
+// 	if (CrossResult.Z < 0.f)
+// 	{
+// 		OutAngleDifference *= -1.f;
+// 	}
+//
+// 	if (OutAngleDifference >= -45.f && OutAngleDifference <= 45.f)
+// 	{
+// 		return BaseGameplayTags::Shared_Status_HitReact_Front;
+// 	}
+// 	else if (OutAngleDifference < -45.f && OutAngleDifference >= -135.f)
+// 	{
+// 		return BaseGameplayTags::Shared_Status_HitReact_Left;
+// 	}
+// 	else if (OutAngleDifference < -135.f || OutAngleDifference > 135.f)
+// 	{
+// 		return BaseGameplayTags::Shared_Status_HitReact_Back;
+// 	}
+// 	else if (OutAngleDifference > 45.f && OutAngleDifference <= 135.f)
+// 	{
+// 		return BaseGameplayTags::Shared_Status_HitReact_Right;
+// 	}
+// 	return FGameplayTag();
+// }
+
+// bool UBaseFunctionLibrary::IsValidBlock(AActor* InAttacker, AActor* InDefender)
+// {
+// 	check(InAttacker && InDefender);
+//
+// 	const float DotResult = FVector::DotProduct(InAttacker->GetActorForwardVector(), InDefender->GetActorForwardVector());
+//
+// 	const FString DebugString = FString::Printf(TEXT("Dot Result : %f %s"), DotResult, DotResult < 0.f ? TEXT("Valid Block") : TEXT("InValid Block"));
+// 	Debug::Print(DebugString, DotResult < 0.f ? FColor::Green : FColor::Red);
+// 	
+// 	return DotResult < -0.1f;
+// }
--- Source/TP_S/Private/Chacracter/Player/BasePlayerCharacter.cpp
+++ Source/TP_S/Private/Chacracter/Player/BasePlayerCharacter.cpp
@@ -7,7 +7,9 @@
 #include "EnhancedInputSubsystems.h"
 #include "AbilitySystem/BaseAbilitySystemComponent.h"
 #include "Camera/CameraComponent.h"
+#include "Components/BaseInputComponent.h"
 #include "Components/CapsuleComponent.h"
+#include "DataAssets/DataAsset_InputConfig.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 
@@ -33,13 +35,64 @@ ABasePlayerCharacter::ABasePlayerCharacter()
 	GetCharacterMovement()->RotationRate = FRotator(0.5f, 500.f, 0.f);
 	GetCharacterMovement()->MaxWalkSpeed = 400.f;
 	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
+	
+}
 
-	/*
-	static ConstructorHelpers::FObjectFinder<USkeletalMesh> MeshAsset(TEXT("/Game/Assets/HeroCharacter/SK_CharM_Barbarous.SK_CharM_Barbarous"));
-	if (MeshAsset.Succeeded())
+void ABasePlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	check(InputConfigDataAsset)
+	ULocalPlayer* LocalPlayer = GetController<APlayerController>()->GetLocalPlayer();
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem =  ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(LocalPlayer);
+
+	check(Subsystem);
+
+	Subsystem->AddMappingContext(InputConfigDataAsset->DefaultMappingContext, 0);
+
+	UBaseInputComponent* BaseInputComponent = CastChecked<UBaseInputComponent>(PlayerInputComponent);
+
+	BaseInputComponent->BindNativeInputAction(InputConfigDataAsset, BaseGamePlayTags::InputTag_Move, ETriggerEvent::Triggered, this, &ThisClass::Input_Move);
+	BaseInputComponent->BindNativeInputAction(InputConfigDataAsset, BaseGamePlayTags::InputTag_Look, ETriggerEvent::Triggered, this, &ThisClass::Input_Look);
+}
+
+void ABasePlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ABasePlayerCharacter::Input_Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D MovementVector = InputActionValue.Get<FVector2D>();
+
+	const FRotator MovementRotation(0.f,Controller->GetControlRotation().Yaw,0.f);
+
+	if (MovementVector.Y != 0.f)
 	{
-		GetMesh()->SetSkeletalMesh(MeshAsset.Object);
-		GetMesh()->SetRelativeLocationAndRotation(FVector(0.f, 0.f, -100.f), FRotator(0.f, -90.f, 0.f));
+		const FVector ForwardDirection = MovementRotation.RotateVector(FVector::ForwardVector);
+
+		AddMovementInput(ForwardDirection,MovementVector.Y);
 	}
-	*/
+
+	if (MovementVector.X != 0.f)
+	{
+		const FVector RightDirection = MovementRotation.RotateVector(FVector::RightVector);
+
+		AddMovementInput(RightDirection,MovementVector.X);
+	}
+}
+
+void ABasePlayerCharacter::Input_Look(const FInputActionValue& InputActionValue)
+{
+	const FVector2D LookAxisVector = InputActionValue.Get<FVector2D>();
+	
+	if (LookAxisVector.X != 0.f)
+	{
+		AddControllerYawInput(LookAxisVector.X);
+	}
+
+	if (LookAxisVector.Y != 0.f)
+	{
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
 }
